e2925f40b4e1821c76024f27ebc15190712cf37a
Parth Agarwal <parthagarwal57@gmail.com>
2021-04-04 12:29:34 +0530

    First Commit

--- app.py
+++ app.py
@@ -1,58 +1,53 @@
-from flask import Flask, render_template, request
-import jsonify
-import requests
+from pywebio.input import *
+from pywebio.output import *
+
 import pickle
 import numpy as np
 import sklearn
 from sklearn.preprocessing import StandardScaler
-app = Flask(__name__)
-model = pickle.load(open('regression.pkl', 'rb'))
-@app.route('/',methods=['GET'])
-def Home():
-    return render_template('index.html')
+model = pickle.load(open('regression_rf.pkl', 'rb'))
 
 
 standard_to = StandardScaler()
-@app.route("/predict", methods=['POST'])
+
 def predict():
+    Year = input("Enter the Model Year：", type=NUMBER)
+    Year = 2021 - Year
+    Present_Price = input("Enter the Present Price(in LAKHS)", type=FLOAT)
+    Kms_Driven = input("Enter the distance it has travelled(in KMS)：", type=FLOAT)
+    Kms_Driven2 = np.log(Kms_Driven)
+    Owner = input("Enter the number of owners who have previously owned it(0 or 1 or 2 or 3)", type=NUMBER)
+    Fuel_Type = select('What is the Fuel Type', ['Petrol', 'Diesel','CNG'])
+    if (Fuel_Type == 'Petrol'):
+        Fuel_Type = 239
 
-    if request.method == 'POST':
-        Year = int(request.form['Year'])
-        Year = 2021 - Year
-        Present_Price = float(request.form['Present_Price'])
-        Kms_Driven = int(request.form['Kms_Driven'])
-        Kms_Driven2 = np.log(Kms_Driven)
-        Owner = int(request.form['Owner'])
-        Fuel_Type = request.form['Fuel_Type']
-        if (Fuel_Type == 'Petrol'):
-            Fuel_Type = 239
+    elif (Fuel_Type == 'Diesel'):
+        Fuel_Type = 60
 
-        elif (Fuel_Type == 'Diesel'):
-            Fuel_Type = 60
-
-        else:
-            Fuel_Type = 2
-        Seller_Type = request.form['Seller_Type']
-        if (Seller_Type == 'Individual'):
-            Seller_Type = 106
-
-        else:
-            Seller_Type = 195
-
-        Transmission = request.form['Transmission']
-        if(Transmission == 'Mannual'):
-            Transmission = 261
-        else:
-            Transmission = 40
-        prediction = model.predict([[Present_Price,Kms_Driven2,Fuel_Type,Seller_Type,Transmission,Owner,Year]])
-        output = round(prediction[0],2)
-        if output<0:
-            return render_template('index.html',prediction_texts = "Sorry you cannot sell this car")
-        else:
-            return render_template('index.html',prediction_text = f"You Can Sell The Car at {output}")
     else:
-        return render_template('index.html')
+        Fuel_Type = 2
+    Seller_Type = select('Are you a dealer or an individual', ['Dealer', 'Individual'])
+    if (Seller_Type == 'Individual'):
+        Seller_Type = 106
+
+    else:
+        Seller_Type = 195
+    Transmission = select('Transmission Type', ['Manual Car', 'Automatic Car'])
+    if (Transmission == 'Manual Car'):
+        Transmission = 261
+    else:
+        Transmission = 40
+
+    prediction = model.predict([[Present_Price, Kms_Driven2, Fuel_Type, Seller_Type, Transmission, Owner, Year]])
+    output = round(prediction[0], 2)
+
+    if output < 0:
+        put_text("Sorry You can't sell this Car")
+
+    else:
+        put_text('You can sell this Car at price:',output)
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    predict()
 
